d650861e548b5487163dbeebd4a720f98f5a5bcf
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2025-01-13 23:35:52 +0700

    day-two_task-1

---
+++ day-two/task-1/index.ts
@@ -0,0 +1,19 @@
+const countFilledValues = (obj: Record<string, unknown>): number => {
+    const filledValuesFilter = Object.values(obj).filter(value => value !== undefined && value !== null && value !== "")
+    return filledValuesFilter.length;
+};
+
+const data = {
+    name: "Pedro",
+    age: 25,
+    address: "",
+    phone: 937-235,
+    email: null,
+    notes: null,
+    salary: 75000,
+    skills: ["JavaScript", "TypeScript", "React"],
+    projects: null,
+    isEmployed: false,
+};
+
+console.log(`Заполненных значений: ${countFilledValues(data)}`);
